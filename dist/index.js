7e6eb839398d68ab889d7cc4151baf9f379673bd
anurag9601 <anuragmishrap13@gmail.com>
2025-08-22 23:37:30 +0530

    some changes

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Hey, anurag everything is working fine.");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline_1 = __importDefault(require("readline"));
+const rl = readline_1.default.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const authenticationQuestion = "Authneticate your github account to proceed further. (y/n)";
+function githubAuthentication() {
+    rl.write("Welcome to AI code reviewer on PR request.");
+    rl.question(authenticationQuestion, (answer) => {
+        if (answer) {
+        }
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hey, anurag everything is working fine.")
+import readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const authenticationQuestion = "Authneticate your github account to proceed further. (y/n)"
+
+function githubAuthentication() {
+    rl.write("Welcome to AI code reviewer on PR request.")
+    rl.question(authenticationQuestion, (answer) => {
+        if(answer ){
+
+        }
+    } )
+}
